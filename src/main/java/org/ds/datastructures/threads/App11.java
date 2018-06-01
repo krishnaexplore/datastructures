a995d7f2ab7b309521f89f1df386fa9d05d22ad8
Krishnaiah Gangaraju <kgangaraju@LPMC-KGANGARAJU.local>
2018-06-01 19:57:01 +0530

    add account class in same App11

--- src/main/java/org/ds/datastructures/threads/App11.java
+++ src/main/java/org/ds/datastructures/threads/App11.java
@@ -9,6 +9,26 @@ import java.util.concurrent.locks.ReentrantLock;
  * 
  * */
 
+class Account11 {
+
+	private int balance = 10000;
+	
+	public void deposit(int amount) {
+		balance += amount;
+	}
+	public void withDraw(int amount) {
+		balance -= amount;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public static void transfer(Account11 acc1, Account11 acc2, int amount) {
+		acc1.withDraw(amount);
+		acc2.deposit(amount);
+	}
+}
+
+
 class Runner11 {
 	
 	private Account11 acc1 = new Account11();
